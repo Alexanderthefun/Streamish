1e8d3686f4082e80da0f460edd8b1c048086e1d9
Alex Fun <alexanderthefun91@gmail.com>
2023-04-12 16:11:58 -0500

    initial commit

---
+++ Repositories/IUserProfileRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Streamish.Models;
+
+namespace Streamish.Repositories
+{
+	public interface IUserProfileRepository
+	{
+		List<UserProfile> GetAll();
+		UserProfile GetById(int id);
+		public UserProfile GetUserByIdWithVideosAndComments(int id);
+		void Add(UserProfile user);
+		void Delete(int id);
+		void Update(UserProfile user);
+		public List<UserProfile> GetAllUsersWithVideos();
+	}
+}
---
+++ Repositories/IVideoRepository.cs
@@ -0,0 +1,19 @@
+﻿using Streamish.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Streamish.Repositories
+{
+	public interface IVideoRepository
+	{
+		void Add(Video video);
+		void Delete(int id);
+		List<Video> GetAll();
+		List<Video> GetAllWithComments();
+		Video GetVideoByIdWithComments(int id);
+		Video GetById(int id);
+		void Update(Video video);
+		public List<Video> Search(string criterion, bool sortDescending);
+		public List<Video> Hottest(DateTime date, bool sortDescending);
+	}
+}
